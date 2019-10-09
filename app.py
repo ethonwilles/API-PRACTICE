8cbe38536b94df986b8f924ba5a54c30f7bd5f46
ethonwilles <ethonwilles@gmail.com>
2019-10-09 10:55:34 -0600

    latest

--- app.py
+++ app.py
@@ -3,3 +3,36 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+
+CORS(app)
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    done = db.Column(db.Boolean)
+
+    def __init__(self, title, done):
+        self.title = title
+        self.done = done
+    
+
+class TodoSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "done")
+    
+
+todo_schema = TodoSchema()
+todos_schema = TodoSchema(many=True)
+
+@app.route("/todos",methods=["GET"])
+def get_todos():
+    all_todos = Todo.query.all()
+    result = todos_schema.dump(all_todos)
+    return jsonify(result)
